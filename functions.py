6badb48879d4d88c77e8bb9201a3351cef10c1bf
navendu <navendu.vyas10@gmail.com>
2024-06-15 14:23:23 +0530

    Initial commit to deploy webapp

---
+++ functions.py
@@ -0,0 +1,13 @@
+def get_todos(filepath="todos.txt"):
+    """ Read a text file and return the list of
+    to-do items.
+    """
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath="todos.txt"):
+    """ Write the to-do item list in text file."""
+    with open('todos.txt', 'w') as file:
+        file.writelines(todos_arg)
